e672b127d5eb4ae0ca016c8e628feffbbe96ef8c
Thiago Ferreira <milovaz@gmail.com>
2022-05-26 18:27:59 -0300

    spike arrays as params

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@
 mod test;
 
 fn main() {
-  println!("The sum of 2 + 2 is {}", sum(2,2));
+  let s = Vec::new();
+  let g = Vec::new();
+  evaluate(s, g);
 }
+
+fn evaluate(secret: Vec<String>, guess: Vec<String>) {
+  println!("Secret: {}", secret);
+  println!("Guess: {}", guess);
+}
+
 
 fn sum(a: i32, b:i32) -> i32 {
   return a + b;
