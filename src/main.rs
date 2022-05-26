94d8e51b1ec3013860999a87bf2b6be5a949c528
HelioAdson <helioadson@gmail.com>
2022-05-26 18:30:38 -0300

    etapa 1

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@
 mod test;
 
 fn main() {
-  println!("The sum of 2 + 2 is {}", sum(2,2));
+  println!("result:", evaluate(['azul', 'verde'], ['vermelho', 'verde']));
 }
 
 fn sum(a: i32, b:i32) -> i32 {
   return a + b;
+}
+
+
+// comparar as duas arrays e retornar se tiver algo igual.
+// verificar o index e ver se estão na mesma posição
+// mesma posição ? -> well placed ++
+// não está na mesma posição ? -> misplaced ++
+
+fn evaluate(array_1, array_2) {
+  well_placed = 0
+  misplaced = 0
 }
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,12 @@
 use super::*;
 
 #[test]
+// fn it_works() {
+//     let result = sum(2, 2);
+//     assert_eq!(result, 4);
+// }
+
 fn it_works() {
-    let result = sum(2, 2);
-    assert_eq!(result, 4);
+    let result = evaluate(['azul', 'verde'], ['vermelho', 'verde']);
+    assert_eq!(result, [1, 0]);
 }
